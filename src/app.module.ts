de194e29ef7c6cd6bcd056812300ce0ce11e0998
Costandache Florin <costandacheflorinadrian@gmail.com>
2023-05-21 10:59:35 +0300

    created the basic form of the recommendations service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecommendationsModule } from './modules/recommendations/recommendations.module';
+import { RecommendationsController } from './modules/recommendations/controllers/recommendation.getTracks.controller';
 
 @Module({
   imports: [RecommendationsModule],
-  controllers: [AppController],
+  controllers: [AppController, RecommendationsController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/modules/recommendations/recommendations.service.ts
+++ src/modules/recommendations/recommendations.service.ts
@@ -1,8 +1,42 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MerchantSkinsService {
-  async createDefaultSkin() {
-    console.log('respect');
+export class RecommandationService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getRecomandation(
+    track: string,
+    artist: string,
+    genres: string[],
+    popularity: string,
+  ) {
+    let BASE_RECOMMENDATION_URL = 'https://api.spotify.com/v1/recommendations';
+
+    const token =
+      'BQDGZ81z15gntPucQe_efc-LJPZvjuIUknd_6h7xLi-YlXgtzj6YTRR3LeIbeDXJc_LVudHMtPZZNXmh7040z6U9a1J2exNoq9LrmIB5dfOtkpUsssYw';
+    const headers = {
+      Authorization: `Bearer ${token}`, // Add the bearer token to the Authorization header
+    };
+
+    if (track) {
+      BASE_RECOMMENDATION_URL += `?tracks=${track}`;
+    }
+    if (artist) {
+      BASE_RECOMMENDATION_URL += `?seed_artists=${artist}`;
+    }
+
+    if (genres && genres.length > 0) {
+      const queryString = genres
+        .map((genre) => `seed_genres=${encodeURIComponent(genre)}`)
+        .join('&');
+      BASE_RECOMMENDATION_URL += `?${queryString}`;
+    }
+
+    if (popularity) {
+      const popularity2 = '100';
+      BASE_RECOMMENDATION_URL += `?max_popularity=${popularity2}`;
+    }
+    return this.httpService.get(`${BASE_RECOMMENDATION_URL}`, { headers });
   }
 }
