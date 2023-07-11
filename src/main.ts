70a2e6112782e5b99dfe3713c772e33d7b36c5f6
Costandache Florin <costandacheflorinadrian@gmail.com>
2023-07-11 19:59:15 +0300

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
+  const config = new DocumentBuilder()
+    .setTitle('Music Pal API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(5000);
 }
 bootstrap();
