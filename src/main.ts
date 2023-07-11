e8b235f4acdc47a0eaa562cdd348700940bba7da
Costandache Florin <costandacheflorinadrian@gmail.com>
2023-07-11 17:39:48 +0300

    modified the cors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: '*',
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
   });
+
   await app.listen(5000);
 }
 bootstrap();
